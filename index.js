ab114d26e6ea7f71aedb493a912885b3000197b3
devwowndev <won3055@gmail.com>
2021-02-27 18:05:19 +0900

    Auth, 로그아웃

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
+const { auth} = require("./middleware/auth");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -22,13 +23,13 @@ mongoose.connect(config.mongoURI,{
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log('err'))
 
-//테스트
+//연결확인
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 //회원가입
-app.post('/register', (req,res) => {
+app.post('/api/users/register', (req,res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면 
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -43,7 +44,7 @@ app.post('/register', (req,res) => {
 })
 
 //로그인
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -69,13 +70,41 @@ app.post('/login', (req, res) => {
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
 
-        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+        // 토큰을 저장한다.  어디에 ?  쿠키 , 로컬스토리지 
         res.cookie("x_auth", user.token)
           .status(200)
-          .json({ loginSuccess: true, userId: user._id })
+          .json({ loginSuccess: true, userId: user._id });
       })
     })
   })
+})
+
+// role 1 어드민    role 2 특정 부서 어드민 
+// role 0 -> 일반유저   role 0이 아니면  관리자 
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  // console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
